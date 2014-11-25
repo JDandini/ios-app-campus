72d2c0cd0cb3b1c7b08317ddcdf6a8b95679ca20
jdandini <jdandini.jc@gmail.com>
2014-11-25 15:22:24 -0600

    playground clase 1

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//print(str)
+var variable:Double =  18
+variable = variable + 1
+//print(variable)
+let tupla = (numero:202,"hola")
+//print(tupla.numero)
+
+var dictionary = ["uno":1, "dos":2, "tres":3]
+let val = dictionary["uno"]!
+//print(dictionary["dos"]!)
+
+let array:[AnyObject] = [1,2,3,4,"3"]
+
+
+let greeting = "¡Este campus rifa!"
+for i in 1 ... 5{
+    println(greeting)
+}
